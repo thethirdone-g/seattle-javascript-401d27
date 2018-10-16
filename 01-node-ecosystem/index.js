20ddce3a2bc9ea8ec259c4e42c4a74be11185925
David Chambers <dlchambersjr@gmail.com>
2018-10-15 23:05:58 -0700

    Finished with tests and index.js

--- 01-node-ecosystem/index.js
+++ 01-node-ecosystem/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
-// get the greet.js 
-require('./lib/greet.js').default;
-require('./lib/arithmetic.js');
+// Require the files to be used
+const greet = require('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
+
+// Call on greet with a number to generate null
+console.log(greet(21));
+
+// Call on greet with 'world' to get hello world
+console.log(greet('world'));
+
+// Call arithmetic with string parameters to generate null
+console.log(arithmetic('a', 'b'));
+
+//Call arithmetic with numbers to add and subtract
+console.log(arithmetic(3, 2));
 
