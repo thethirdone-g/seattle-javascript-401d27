b09f051b822fa8baad401460e5308cdbe2725ea1
George McCadden III <thethirdone.g@gmail.com>
2018-10-16 13:13:11 -0700

    Still working on this lab from yesterday

--- 01-node-ecosystem/lab/lib/arithmetic.js
+++ 01-node-ecosystem/lab/lib/arithmetic.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = exports = (numOne, numTwo) => {
-    if ()
-}
+module.exports = exports = {
+    add: (a, b) => {
+        if (typeof a === 'number' && typeof b === 'number') {
+            return a + b;
+        } else {
+            return null;
+        }
+    }
+    sub: (a, b) => {
+        if (typeof a === 'number' && typeof b === 'number') {
+            return a - b;
+        } else {
+            return null;
+        }
+    }
+};
