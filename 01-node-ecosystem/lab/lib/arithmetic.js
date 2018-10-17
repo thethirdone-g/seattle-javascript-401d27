9a022da5c1952b227ce4cdd3de14a59fa003d63c
George McCadden III <thethirdone.g@gmail.com>
2018-10-17 13:38:17 -0700

    My computer crashed so I don't know what these file changes are

--- 01-node-ecosystem/lab/lib/arithmetic.js
+++ 01-node-ecosystem/lab/lib/arithmetic.js
@@ -1,18 +1,18 @@
 'use strict';
 
 module.exports = exports = {
-    add: (a, b) => {
-        if (typeof a === 'number' && typeof b === 'number') {
-            return a + b;
-        } else {
-            return null;
-        }
+  add: (a, b) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a + b;
+    } else {
+      return null;
     }
-    sub: (a, b) => {
-        if (typeof a === 'number' && typeof b === 'number') {
-            return a - b;
-        } else {
-            return null;
-        }
+  },
+  sub: (a, b) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a - b;
+    } else {
+      return null;
     }
+  },
 };
