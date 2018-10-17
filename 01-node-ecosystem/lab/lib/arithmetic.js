71e9e95bc9608142e5e8f39252ddf2a853cba8d2
George McCadden III <thethirdone.g@gmail.com>
2018-10-17 13:38:17 -0700

    My computer crashed so I don't know what these file changes are

--- 01-node-ecosystem/lab/lib/arithmetic.js
+++ 01-node-ecosystem/lab/lib/arithmetic.js
@@ -1,7 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 module.exports = exports = {
   add: (a, b) => {
     if (typeof a === 'number' && typeof b === 'number') {
@@ -18,26 +16,25 @@ module.exports = exports = {
     }
   },
 };
-=======
+
 module.exports = exports = (numOne, numTwo) => {
     if ()
 }
->>>>>>> I need a little help with testing but I followed demo code
-=======
+
 module.exports = exports = {
-    add: (a, b) => {
-        if (typeof a === 'number' && typeof b === 'number') {
-            return a + b;
-        } else {
-            return null;
-        }
+  add: (a, b) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a + b;
+    } else {
+      return null;
     }
-    sub: (a, b) => {
-        if (typeof a === 'number' && typeof b === 'number') {
-            return a - b;
-        } else {
-            return null;
-        }
+  },
+  sub: (a, b) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a - b;
+    } else {
+      return null;
     }
-};
->>>>>>> Still working on this lab from yesterday
+  }
+
+}
