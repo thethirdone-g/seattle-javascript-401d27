624bfac60b9369dff10e41717e44103850883ab4
George McCadden III <thethirdone.g@gmail.com>
2018-10-18 16:32:32 -0700

    From the command line I can now read and manipulate a file

--- 04-binary-data/lab-04/index.js
+++ 04-binary-data/lab-04/index.js
@@ -7,7 +7,7 @@ class Bitmap {
     this.file = filePath;
   }
   // buffer is a datatype that is retrieved by using fs.readfile
-  // parse is intended to parse the file buffer's contents
+  // parse(buffer) is intended to parse the file buffer's contents
   parse(buffer) {
     this.type = buffer.toString('utf-8', 0, 2);
   }
@@ -18,9 +18,36 @@ class Bitmap {
     this.newFile = this.file.replace(/\.bmp/, `.${callback}.bmp`);
   }
 }
+const transformGreyscale = bmp => {
+  console.log('Transforming bitmap into greyscale', bmp);
+};
 
 // creating an object containing different methods for transformation
 const transformDictionary = {
-  // need to make a greyscale transformation function in the future
-  // greyscale: transformGreyScale;
+  // To Do : need to make a greyscale transformation function in the future
+  greyscale: transformGreyscale,
 };
+
+const readAndTransform = () => {
+  // if breaks, try fs.readFile(file, (err, buffer) => {
+  fs.readFile(file, (err, buffer) => {
+    if (err) {
+      throw err;
+    }
+    baldy.parse(buffer);
+
+    // need to create the callback function
+    baldy.transform(callback);
+
+    fs.writeFile(baldy.newFile, baldy.buffer, (err, out) => {
+      if (err) throw err;
+      console.log(`BitMap tranformed: ${baldy.newFile}`);
+    });
+  });
+};
+
+const [file, callback] = process.argv.slice(2);
+
+let baldy = new Bitmap(file);
+
+readAndTransform();
